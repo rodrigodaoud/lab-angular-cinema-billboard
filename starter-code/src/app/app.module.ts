c60e05ba3f8187e100fc8d692aa8ad9d4b2833aa
Rodrigo Daoud <rodaoud93@gmail.com>
2018-02-21 20:04:47 +0100

    iteration 2 - 1st part

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { MyHomeComponentComponent } from './components/my-home-component/my-home-component.component';
+import { MyMovieComponentComponent } from './components/my-movie-component/my-movie-component.component';
 
 import { MovieServiceService } from 'app/services/movie-service.service';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponentComponent },
+  { path: 'movie', component: MyMovieComponentComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponentComponent,
+    MyMovieComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [MovieServiceService],
   bootstrap: [AppComponent]
